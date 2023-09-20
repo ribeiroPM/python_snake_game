6d97312f98399054c35e56c703d80f89fa650eb0
Paulao <paulomooribeiro@gmail.com>
2023-09-20 18:07:41 -0300

    docs: update main.py

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ from pygame.locals import *
 
 pygame.init()
 
+screen = pygame.display.set_mode((400, 400))
+
+run = True
+while run:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			run = False
+
+
+	pygame.display.update()
+
 
 pygame.quit()
