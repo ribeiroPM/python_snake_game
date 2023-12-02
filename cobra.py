133c82aebe79f212ed9e26a85f7e6532982df9c3
Paulao <paulomooribeiro@gmail.com>
2023-12-01 22:58:49 -0300

    atualizaç ao geral

---
+++ cobra.py
@@ -0,0 +1,164 @@
+import pygame
+from pygame.locals import *
+from os import system
+class Cobra:
+	def __init__(self, tamanho=30, pos_x=30, pos_y=30):
+		self.quantidade_de_partes = 15
+		self.tamanho = tamanho
+		self.pos_x_inicial = pos_x
+		self.pos_y_inicial = pos_y
+		self.posicoes = [{"x": x+self.tamanho, "y": self.pos_y_inicial+self.tamanho, "direcao_antiga": "direita", "direcao_nova": "direita"} for x in range(self.pos_x_inicial, self.pos_x_inicial+self.quantidade_de_partes)] 
+		self.cor = [233, 0, 0]
+		self.cor_cabeca = [0, 233, 0]
+		self.cor_rabo = [0, 0, 233]
+		self.direcoes = {K_a: "esquerda", K_d: "direita", K_w: "cima", K_s: "baixo"} # 1 para direita; 2 para esquerda; 3 para cima; e 4 para "baixo"
+		self.direcao = "direita"
+		self.direcao_antiga = None
+		self.estado = True
+
+		# Importação e Transformação dos Sprites
+		self.imagens = {"tronco": pygame.image.load("tronco.png").convert_alpha(),
+						"cabeca": pygame.image.load("cabeca.png").convert_alpha(),
+						"rabo": pygame.image.load("rabo.png").convert_alpha(),
+						"dobrada": pygame.image.load("dobrada.png").convert_alpha(),
+						"dobrada_2": pygame.image.load("dobrada_2.png").convert_alpha()
+						}
+		self.imagens_transformadas = {}
+		
+		# Define os Sprites da Cabeca 
+		self.direcoes_cabeca_rabo = {"esquerda": 180, "direita": 0, "cima": 90, "baixo": -90}
+
+
+		# Define se será exibido as caixas de colisão
+		self.exibicao = True
+
+	def update(self, surface):
+		# Possibilita aumentar ou diminuir o tamanho dos sprites a qualquer instante
+		self.imagens_transformadas = {x: pygame.transform.scale(y, (self.tamanho, self.tamanho)) for x, y in self.imagens.items()}
+
+		# Guarda a antiga posição 0 (cabeça)
+		antiga_cabeca = self.posicoes[0].copy()
+		# Atualiza para a posição 0 para a direção escolhida
+		if self.direcao == "esquerda":
+			self.posicoes[0]["x"] -= self.tamanho
+		elif self.direcao == "direita":
+			self.posicoes[0]["x"] += self.tamanho
+		elif self.direcao == "cima":
+			self.posicoes[0]["y"] -= self.tamanho
+		elif self.direcao == "baixo":
+			self.posicoes[0]["y"] += self.tamanho
+
+		# Atualiza o restante do corpo (cada parte assumindo a antiga posição da cabeça)
+		for posicao in self.posicoes[1:]:
+			posicao_atual = posicao.copy()
+			posicao["x"] = antiga_cabeca["x"]
+			posicao["y"] = antiga_cabeca["y"]
+			posicao["direcao_antiga"] = antiga_cabeca["direcao_antiga"]
+			posicao["direcao_nova"] = antiga_cabeca["direcao_nova"]
+			antiga_cabeca = posicao_atual
+
+		# Desenha cada parte do corpo
+		for num_pos, posicao_nova in enumerate(self.posicoes):
+			posicao_x = posicao_nova["x"]
+			posicao_y = posicao_nova["y"]
+			direcao_antiga = posicao_nova["direcao_antiga"]
+			direcao_nova = posicao_nova["direcao_nova"]
+			if num_pos == 0: # Gerencia a parte da Cabeca
+				cor = self.cor_cabeca
+				# Roda o Sprite da Cabeça conforme a Direção que a cobrinha está
+				imagem = pygame.transform.rotate(self.imagens_transformadas["cabeca"], self.direcoes_cabeca_rabo[direcao_nova])
+			elif num_pos == len(self.posicoes)-1: # Gerencia a parte do Rabo
+				cor = self.cor_rabo
+				# Roda o Sprite do Rabo conforme a Direção que a cobrinha está
+				direcao_do_rabo = direcao_antiga if posicao_x != posicao_y else direcao_nova
+				imagem = pygame.transform.rotate(self.imagens_transformadas["rabo"], self.direcoes_cabeca_rabo[direcao_do_rabo])
+			else: # Gerencia o restante do Corpo
+				cor = self.cor
+				if direcao_antiga == direcao_nova: # Checa se está parte do corpo esta na mesma direção
+					if direcao_antiga in ["esquerda", "direita"]: # Se está indo para a Direita ou Esquerda, tronco na Horizontal
+						imagem = self.imagens_transformadas["tronco"]
+					else:
+						imagem = pygame.transform.rotate(self.imagens_transformadas["tronco"], 90) # Se está indo para Cima ou Para Baixo, tronco rotacionado em 90 graus
+				else: # Se as direções Nova e Antiga forem diferentes, ocorrerá uma dobra
+					# Possivéis dobradas:
+						# Baixo    > Direita;
+						# Baixo    > Esquerda;
+						# Cima     > Direita;
+						# Cima     > Esquerda;
+						# Direita  > Baixo;
+						# Direita  > Cima;
+						# Esquerda > Baixo;
+						# Esquerda > Cima.
+					if (direcao_antiga == "direita" and direcao_nova == "cima") or (direcao_antiga == "baixo" and direcao_nova == "esquerda"):
+						imagem = self.imagens_transformadas["dobrada"]
+					elif (direcao_antiga == "direita" and direcao_nova == "baixo") or (direcao_antiga == "cima" and direcao_nova == "esquerda"):
+						imagem = self.imagens_transformadas["dobrada_2"]
+					elif (direcao_antiga == "esquerda" and direcao_nova == "baixo") or (direcao_antiga == "cima" and direcao_nova == "direita"):
+						imagem = pygame.transform.rotate(self.imagens_transformadas["dobrada"], 180)
+					else:
+						imagem = pygame.transform.rotate(self.imagens_transformadas["dobrada_2"], 180)
+
+			if self.exibicao: # Mostra ou Não a área de contato da Cobra
+				pygame.draw.rect(surface, self.cor, (posicao_x, posicao_y, self.tamanho-1, self.tamanho-1))
+
+			# Desenha o Sprite Selecionado
+			surface.blit(imagem, (posicao_x, posicao_y))
+
+		# Após Atualizar e Desenhar todas as partes, o corpo entrará na mesma direção
+		self.posicoes[0]["direcao_antiga"] = self.posicoes[0]["direcao_nova"]
+
+		# Verifica se a Cabeça acertou alguma parte do Corpo (morte)
+		if self.posicoes[0] in self.posicoes[1:]:
+			self.estado = False
+
+		
+	def para_esquerda(self):
+		self.direcao_antiga = self.direcao
+		self.direcao = "esquerda" if self.direcao != "direita" else "direita"
+		if self.direcao != self.direcao_antiga:
+			self.posicoes[0]["direcao_antiga"] = self.direcao_antiga
+			self.posicoes[0]["direcao_nova"] = self.direcao
+	
+
+	def para_direita(self):
+		self.direcao_antiga = self.direcao
+		self.direcao = "direita" if self.direcao != "esquerda" else "esquerda"
+		if self.direcao != self.direcao_antiga:
+			self.posicoes[0]["direcao_antiga"] = self.direcao_antiga
+			self.posicoes[0]["direcao_nova"] = self.direcao
+	
+
+	def para_cima(self):
+		self.direcao_antiga = self.direcao
+		self.direcao = "cima" if self.direcao != "baixo" else "baixo"
+		if self.direcao != self.direcao_antiga:
+			self.posicoes[0]["direcao_antiga"] = self.direcao_antiga
+			self.posicoes[0]["direcao_nova"] = self.direcao
+	
+
+	def para_baixo(self):
+		self.direcao_antiga = self.direcao
+		self.direcao = "baixo" if self.direcao != "cima" else "cima"
+		if self.direcao != self.direcao_antiga:
+			self.posicoes[0]["direcao_antiga"] = self.direcao_antiga
+			self.posicoes[0]["direcao_nova"] = self.direcao
+
+	def get_infos(self, surface):
+		system("cls")
+
+		for n_parte, parte in enumerate(self.posicoes):
+			pos_x = parte["x"]
+			pos_y = parte["y"]
+			direcao_antiga = parte["direcao_antiga"]
+			direcao_nova = parte["direcao_nova"]
+
+			# print(f"Parte: {n_parte}\nx: {pos_x}\ny: {pos_y}\nAntiga: {direcao_antiga}\nNova: {direcao_nova}\n")
+		print(90)
+		surface.blit(pygame.transform.rotate(self.imagens_transformadas["dobrada"], 90), (100, 100))
+		print(180)
+		surface.blit(pygame.transform.rotate(self.imagens_transformadas["dobrada"], 180), (100, 150))
+		print(270)
+		surface.blit(pygame.transform.rotate(self.imagens_transformadas["dobrada"], 270), (100, 200))
+		print(360)
+		surface.blit(pygame.transform.rotate(self.imagens_transformadas["dobrada"], 360), (100, 250))
+
--- main.py
+++ main.py
@@ -1,111 +1,94 @@
 import pygame
 from pygame.locals import *
-from random import randint
+from cobra import Cobra
+from time import strftime
+a = 0
+s = strftime("%S")
+def contar_fps():
+	global s, a
+	if s != strftime("%S"):
+		s = strftime("%S")
+		print(a)
+		a = 0
 
-# O jogo ainda esta em processo de desenvolvimento, atualizacoes em breve
 
+
+# Constantes referentes a janela do jogo
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
+SCREEN_COLOR = (255, 255, 255)
 
 pygame.init()
+# Inicia uma janela
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-screen_x = 700
-screen_y = 500
-screen = pygame.display.set_mode((screen_x, screen_y))
-screen_bg = (100, 100, 100)
+# Definições para o tempo de atualização da tela
+# clock = pygame.time.Clock()
+# fps = 10
+limite_atualizacao_tela = 150000
+contador_atualizacao = 0
+# Instancia a cobrinha
+cobra_1 = Cobra()
+cobra_2 = Cobra(tamanho=10, pos_y=100)
+cobra_3 = Cobra(pos_y=200)
+cobra_4 = Cobra(pos_y=300)
 
-clock = pygame.time.Clock()
-fps = 20
+# Mostra todas as informacoes da cobra
+show_infos = False
+# pausa
+pause = False
 
-# Variaveis a respeito da cobrinha
-cobrinha = {"cores": [[255, 255, 255], [255, 0, 255]], "pos": [[x, 30] for x in range(50, 20, -10)], "tamanho": 10, "design": 1}
-movimentos = {K_a: [0, -1], K_d: [0, 1], K_w: [1, -1], K_s: [1, 1]}
-sentido_da_cobrinha = 0 # Gerencia em qual eixo a cobrinha vai se deslocar. 0=x, 1=y
-movimento_da_cobrinha = 1 # Gerencia se ela vai se movimentar para baixo/cima ou esquerda/direita.
-antigo_movimento_da_cobrinha = 0
-
-# Variaveis a respeito da frutinha
-def gera_fruta_nova():
-	return (randint(0, (screen_x-1)//10)*10, randint(0, (screen_y-1)//10)*10)
-fruta = {"tem_fruta": True, "pos": gera_fruta_nova(), "cor": [255, 0, 0]}
-print(fruta["pos"])
-
-
-
-def teve_colisao(ponto_a, ponto_b):
-	ponto = pygame.draw.rect(screen, (0, 0, 0), (ponto_a[0], ponto_a[1], 1, 1))
-	if ponto.collidepoint(ponto_b[0], ponto_b[1]):
-		return True
 
 run = True
 while run:
-	clock.tick(fps)
+	# Controle de atualizações por segundo
+	# clock.tick(fps)
+	# Laço de eventos de entrada (teclado/mouse)
 	for event in pygame.event.get():
+		# Verifica se o [x] da janela foi clicado
 		if event.type == QUIT:
 			run = False
 		if event.type == KEYDOWN:
-			tecla = event.key
-			if tecla in movimentos:
-				# Define o movimento e o sentido com base na tecla pressionada
-				if tecla == K_a and antigo_movimento_da_cobrinha == K_d:
-					# Cadeia de if's responsáveis por evitar movimentos "para trás", que resultariam em game_over
-					continue
-				elif tecla == K_d and antigo_movimento_da_cobrinha == K_a:
-					continue
-				elif tecla == K_s and antigo_movimento_da_cobrinha == K_w:
-					continue
-				elif tecla == K_w and antigo_movimento_da_cobrinha == K_s:
-					continue	
-				sentido_da_cobrinha = movimentos[tecla][0]
-				movimento_da_cobrinha = movimentos[tecla][1]
-				antigo_movimento_da_cobrinha = tecla
+			key = event.key
+			if key == K_a:
+				cobra_1.para_esquerda()
+			elif key == K_d:
+				cobra_1.para_direita()
+			elif key == K_w:
+				cobra_1.para_cima()
+			elif key == K_s:
+				cobra_1.para_baixo()
+			elif key == K_SPACE:
+				pause = False if pause == True else True
+			elif key == K_q:
+				show_infos = False if show_infos == True else True
+			elif key == K_p:
+				cobra_1.tamanho += 1
+	if not pause:
+		if contador_atualizacao == limite_atualizacao_tela:
+			# Reseta a tela para a cor inicial
+			screen.fill(SCREEN_COLOR)
+			cobra_1.update(screen)
 
+			cobra_2.update(screen)
+			cobra_3.update(screen)
+			cobra_4.update(screen)
 
-
-	# Salva a posicão antiga da cabeça da cobrinha
-	cabeca_da_cobrinha = cobrinha["pos"][0][:]
-	cobrinha["pos"][0][sentido_da_cobrinha] += movimento_da_cobrinha*10
-
-	# Verifica se a cobrinha atingiu os limites da tela
-	if cobrinha["pos"][0][0] > screen_x:
-		cobrinha["pos"][0][0] = 0
-	if cobrinha["pos"][0][1] > screen_y:
-		cobrinha["pos"][0][1] = 0
-	if cobrinha["pos"][0][0] < 0:
-		cobrinha["pos"][0][0] = screen_x
-	if cobrinha["pos"][0][1] < 0:
-		cobrinha["pos"][0][1] = screen_y
-
-	screen.fill(screen_bg)
-
-	# Atualiza o restante do corpo da cobrinha
-	for n_parte_da_cobrinha in range(1, len(cobrinha["pos"])):
-		posicao_atual = cobrinha["pos"][n_parte_da_cobrinha][:]
-		cobrinha["pos"][n_parte_da_cobrinha] = cabeca_da_cobrinha
-		cabeca_da_cobrinha = posicao_atual
-		
-
-	# Desenha a cobrinha na tela
-	for numero_da_parte, parte_da_cobrinha in enumerate(cobrinha["pos"]):
-		x_pos = parte_da_cobrinha[0]
-		y_pos = parte_da_cobrinha[1]
-		tamanho = cobrinha["tamanho"] - cobrinha["design"]
-		cor = cobrinha["cores"][0] if numero_da_parte != 0 else cobrinha["cores"][1]
-		pygame.draw.rect(screen, cor, (x_pos, y_pos, tamanho, tamanho))
-		if teve_colisao(cobrinha["pos"][0], parte_da_cobrinha) and numero_da_parte > 0:
-			run = False
-
-
-	# Desenha a fruta na tela
-	fruta_pos_x = fruta["pos"][0]
-	fruta_pos_y = fruta["pos"][1]
-	pygame.draw.rect(screen, fruta["cor"], (fruta_pos_x, fruta_pos_y, 10, 10))
-
-	if teve_colisao(cobrinha["pos"][0], fruta["pos"]):
-		fruta["pos"] = gera_fruta_nova()
-		cobrinha["pos"].append([0, 0])
-		print(fruta["pos"])
-
-
-	pygame.display.update()
+			if not cobra_1.estado:
+				run = False
+			contador_atualizacao = 0
+			if show_infos:
+				cobra_1.get_infos(screen)
+			""" # Bloco para contar quantas atualizações estamos tendo da cobrinha
+			a+=1
+			contar_fps() 
+			"""
+			# Atualiza a tela para aplicar possiveis alterações
+			pygame.display.update()
+			
+		else:
+			contador_atualizacao += 1
 
 
 pygame.quit()
+
